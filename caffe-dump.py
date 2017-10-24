26e0bdc8fc0f0f8f8029020866e30a40ddff26a9
Yassine Benyahia <yassine.benyahia1@gmail.com>
2017-10-24 17:09:28 +0200

    Update caffe-dump.py

--- caffe-dump.py
+++ caffe-dump.py
@@ -9,7 +9,7 @@ f_nvidia_caffe = h5py.File(filename2, 'r')
 
 for i in f_nvidia_caffe['lyr']:
     if np.linalg.norm(list(f_nvidia_caffe['lyr'][str(i)]['output'])) - np.linalg.norm(list(f_caffe['lyr'][str(i)]['output']))>1000:
-        print("warn")
+        print(i)
         break
 
 # To see the output of the last layer
